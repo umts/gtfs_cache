6d832b0bfbad02b8ec6ee3b812197577188618e2
benmelz <ben@melz.me>
2026-05-18 15:44:43 -0400

    finish up specs

--- spec/lib/gtfs_cache/store_spec.rb
+++ spec/lib/gtfs_cache/store_spec.rb
@@ -5,114 +5,87 @@ require "gtfs_cache/store"
 RSpec.describe GtfsCache::Store do
   include ActiveSupport::Testing::TimeHelpers
 
-  before do
-    freeze_time
-    allow(GtfsCache::Remote).to receive_messages(gtfs_schedule: nil,
-                                                 gtfs_realtime_alerts: nil,
-                                                 gtfs_realtime_trip_updates: nil)
+  shared_examples "a cached remote value" do |remote_key: nil, ttl: nil|
+    before do
+      freeze_time
+      allow(GtfsCache::Remote).to receive_messages(gtfs_schedule: nil,
+                                                   gtfs_realtime_alerts: nil,
+                                                   gtfs_realtime_trip_updates: nil)
+      allow(GtfsCache::Remote).to receive(remote_key).and_return("data 1", "data 2", nil)
+    end
+
+    context "when called initially" do
+      it "returns nil" do
+        expect(subject).to be_nil
+      end
+    end
+
+    context "when called after an update check with no existing data" do
+      before { described_class.check_for_updates }
+
+      it "returns freshly fetched data" do
+        expect(subject).to eq("data 1")
+      end
+    end
+
+    context "when called after an update check with fresh data" do
+      before do
+        described_class.check_for_updates
+        travel ttl
+        described_class.check_for_updates
+      end
+
+      it "returns the still fresh data" do
+        expect(subject).to eq("data 1")
+      end
+    end
+
+    context "when called after an update check with stale data" do
+      before do
+        described_class.check_for_updates
+        travel ttl
+        described_class.check_for_updates
+        travel 1.second
+        described_class.check_for_updates
+      end
+
+      it "returns freshly fetched data" do
+        expect(subject).to eq("data 2")
+      end
+    end
+
+    context "when called after an update check with stale data that failed" do
+      before do
+        described_class.check_for_updates
+        travel ttl
+        described_class.check_for_updates
+        travel 1.second
+        described_class.check_for_updates
+        travel ttl + 1.second
+        described_class.check_for_updates
+      end
+
+      it "returns stale data" do
+        expect(subject).to eq("data 2")
+      end
+    end
   end
 
   describe ".gtfs_schedule" do
     subject(:call) { described_class.gtfs_schedule }
 
-    before { allow(GtfsCache::Remote).to receive(:gtfs_schedule).and_return("data 1", "data 2", nil) }
-
-    context "without checking for updates" do
-      it "returns nil" do
-        expect(call).to be_nil
-      end
-    end
-
-    context "when initial data has been fetched" do
-      before { described_class.check_for_updates }
-
-      it "returns the stored data" do
-        expect(call).to eq("data 1")
-      end
-    end
-
-    context "when updates have been checked less than 1 day after the last" do
-      before do
-        described_class.check_for_updates
-        travel 23.hours + 59.minutes + 59.seconds
-        described_class.check_for_updates
-      end
-
-      it "returns the initial stored data" do
-        expect(call).to eq("data 1")
-      end
-    end
-
-    context "when updates have been checked more than 1 day after the last" do
-      before do
-        described_class.check_for_updates
-        travel 23.hours + 59.minutes + 59.seconds
-        described_class.check_for_updates
-        travel 1.minute
-        described_class.check_for_updates
-      end
-
-      it "returns refreshed data" do
-        expect(call).to eq("data 2")
-      end
-    end
-
-    context "when we have stale data but didnt get anything back from the remote" do
-      before do
-        described_class.check_for_updates
-        travel 23.hours + 59.minutes + 59.seconds
-        described_class.check_for_updates
-        travel 1.minute
-        described_class.check_for_updates
-        travel 24.hours
-        described_class.check_for_updates
-      end
-
-      it "returns stale data" do
-        expect(call).to eq("data 2")
-      end
-    end
+    it_behaves_like "a cached remote value", remote_key: :gtfs_schedule, ttl: 1.day
   end
 
   describe ".gtfs_realtime_alerts" do
     subject(:call) { described_class.gtfs_realtime_alerts }
 
-    context "when the store is empty" do
-      it "returns nil" do
-        expect(call).to be_nil
-      end
-    end
-
-    context "when the store has data" do
-      before do
-        allow(GtfsCache::Remote).to receive(:gtfs_realtime_alerts).and_return("stored data")
-        described_class.check_for_updates
-      end
-
-      it "returns the stored data" do
-        expect(call).to eq("stored data")
-      end
-    end
+    it_behaves_like "a cached remote value", remote_key: :gtfs_realtime_alerts, ttl: 10.seconds
   end
 
   describe ".gtfs_realtime_trip_updates" do
     subject(:call) { described_class.gtfs_realtime_trip_updates }
 
-    context "when the store is empty" do
-      it "returns nil" do
-        expect(call).to be_nil
-      end
-    end
-
-    context "when the store has data" do
-      before do
-        allow(GtfsCache::Remote).to receive(:gtfs_realtime_trip_updates).and_return("stored data")
-        described_class.check_for_updates
-      end
-
-      it "returns the stored data" do
-        expect(call).to eq("stored data")
-      end
-    end
+    it_behaves_like "a cached remote value", remote_key: :gtfs_realtime_trip_updates, ttl: 10.seconds
   end
 end
