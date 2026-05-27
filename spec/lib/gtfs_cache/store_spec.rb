7832a6c56be67bb2e11b2af8a330f27b16cf719f
benmelz <ben@melz.me>
2026-05-27 14:18:40 -0400

    reimplement store writer specs

--- spec/lib/gtfs_cache/store_spec.rb
+++ spec/lib/gtfs_cache/store_spec.rb
@@ -5,7 +5,7 @@ require "gtfs_cache/store"
 RSpec.describe GtfsCache::Store do
   include ActiveSupport::Testing::TimeHelpers
 
-  shared_examples "a cache entry" do |key: nil|
+  shared_examples "it reads from the cache" do |key: nil|
     context "when there is no data in redis" do
       before { redis.set("#{key}:expires", "not valid") }
 
@@ -35,27 +35,127 @@ RSpec.describe GtfsCache::Store do
     end
   end
 
+  shared_examples "it writes to the cache" do |key: nil, ttl: nil|
+    let(:local_data) { remote_data }
+
+    before { freeze_time }
+
+    context "when the remote responds successfully" do
+      before { request_stub.to_return(status: 200, body: remote_data) }
+
+      context "with nothing in the cache" do
+        it "caches the remote data with control information" do
+          subject
+          expect(redis.mget("#{key}:data", "#{key}:expires")).to eq([local_data, ttl.from_now.to_i.to_s])
+        end
+      end
+
+      context "with incomplete data in the cache" do
+        before { redis.set("#{key}:expires", Time.current.to_i) }
+
+        it "caches the remote data with control information" do
+          subject
+          expect(redis.mget("#{key}:data", "#{key}:expires")).to eq([local_data, ttl.from_now.to_i.to_s])
+        end
+      end
+
+      context "with stale data in the cache" do
+        before { redis.mset("#{key}:data", "stale data", "#{key}:expires", 1.second.ago.to_i) }
+
+        it "caches the remote data with control information" do
+          subject
+          expect(redis.mget("#{key}:data", "#{key}:expires")).to eq([local_data, ttl.from_now.to_i.to_s])
+        end
+      end
+
+      context "with fresh data in the cache" do
+        before { redis.mset("#{key}:data", "fresh data", "#{key}:expires", Time.current.to_i) }
+
+        it "keeps the fresh data" do
+          subject
+          expect(redis.mget("#{key}:data", "#{key}:expires")).to eq(["fresh data", Time.current.to_i.to_s])
+        end
+      end
+    end
+
+    context "when the remote responds with an error" do
+      before { request_stub.to_return(status: 500) }
+
+      context "with nothing in the cache" do
+        it "does nothing and does not raise an error" do
+          subject
+          expect(redis.mget("#{key}:data", "#{key}:expires")).to all(be_nil)
+        end
+      end
+
+      context "with stale data" do
+        before { redis.mset("#{key}:data", "some data", "#{key}:expires", 1.second.ago.to_i) }
+
+        it "keeps the stale data around" do
+          subject
+          expect(redis.mget("#{key}:data", "#{key}:expires")).to eq(["some data", 1.second.ago.to_i.to_s])
+        end
+      end
+    end
+  end
+
   describe ".gtfs_schedule" do
     subject { described_class.gtfs_schedule }
 
-    it_behaves_like "a cache entry", key: "gtfs_schedule"
+    it_behaves_like "it reads from the cache", key: "gtfs_schedule"
   end
 
   describe ".gtfs_schedule_routes" do
     subject { described_class.gtfs_schedule_routes }
 
-    it_behaves_like "a cache entry", key: "gtfs_schedule_routes"
+    it_behaves_like "it reads from the cache", key: "gtfs_schedule_routes"
   end
 
   describe ".gtfs_realtime_alerts" do
     subject { described_class.gtfs_realtime_alerts }
 
-    it_behaves_like "a cache entry", key: "gtfs_realtime_alerts"
+    it_behaves_like "it reads from the cache", key: "gtfs_realtime_alerts"
   end
 
   describe ".gtfs_realtime_trip_updates" do
     subject { described_class.gtfs_realtime_trip_updates }
 
-    it_behaves_like "a cache entry", key: "gtfs_realtime_trip_updates"
+    it_behaves_like "it reads from the cache", key: "gtfs_realtime_trip_updates"
+  end
+
+  describe ".check_for_updates" do
+    subject { described_class.check_for_updates }
+
+    before do
+      allow(CREDENTIALS).to receive(:swiftly_api_key).and_return("test-swiftly-api-key")
+      stub_request(:get, /.*/).to_return(status: 404)
+    end
+
+    it_behaves_like "it writes to the cache", key: "gtfs_schedule", ttl: 1.day do
+      let(:request_stub) { stub_request(:get, "https://www.pvta.com/g_trans/google_transit.zip") }
+      let(:remote_data) { file_fixture("schedule1.zip").read }
+    end
+
+    it_behaves_like "it writes to the cache", key: "gtfs_schedule_routes", ttl: 1.day do
+      let(:request_stub) { stub_request(:get, "https://www.pvta.com/g_trans/google_transit.zip") }
+      let(:remote_data) { file_fixture("schedule1.zip").read }
+      let(:local_data) { file_fixture("schedule1/routes.txt").read }
+    end
+
+    it_behaves_like "it writes to the cache", key: "gtfs_realtime_alerts", ttl: 10.seconds do
+      let(:request_stub) do
+        stub_request(:get, "https://api.goswift.ly/real-time/pioneer-valley-pvta/gtfs-rt-alerts/v2")
+          .with(headers: { "Authorization" => "test-swiftly-api-key" })
+      end
+      let(:remote_data) { "protobuf data" }
+    end
+
+    it_behaves_like "it writes to the cache", key: "gtfs_realtime_trip_updates", ttl: 10.seconds do
+      let(:request_stub) do
+        stub_request(:get, "https://api.goswift.ly/real-time/pioneer-valley-pvta/gtfs-rt-trip-updates")
+          .with(headers: { "Authorization" => "test-swiftly-api-key" })
+      end
+      let(:remote_data) { "protobuf data" }
+    end
   end
 end
